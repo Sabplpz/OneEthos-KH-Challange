4019967d9dc3f8bea2ba119f82c8993d45269481
Sabplpz <sabrinalpz1994@gmail.com>
2025-10-25 15:59:09 -0400

    fixing bugs on the agent

--- financial_agent/agent.py
+++ financial_agent/agent.py
@@ -1,8 +1,8 @@
 from google.adk.agents.llm_agent import Agent
 
-financial_agent = Agent(
+root_agent = Agent(
     model='gemini-2.5-flash',
     name='financial_agent',
     description='A helpful assistant for user financial guidance.',
-    instruction='Answer user questions to the best of your knowledge about financial matters and create plans for them to achive financial goals.',
+    instruction='Answer user questions to the best of your knowledge about financial matters and create plans for them to achieve financial goals.',
 )
