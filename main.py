112cdf150addf34a50763990f89215a6f0d52694
Sabplpz <sabrinalpz1994@gmail.com>
2025-10-25 15:26:59 -0400

    additional changes to fully implement flask

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/features/financial_planning.html')
+def financial_planning():
+    # This function tells Flask to render the specific template
+    # Flask looks in the 'templates/features/' folder
+    return render_template('features/financial_planning.html')
+
+@app.route('/features/ai_coach.html')
+def ai_coach():
+    # This function tells Flask to render the specific template
+    # Flask looks in the 'templates/features/' folder
+    return render_template('features/ai_coach.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
